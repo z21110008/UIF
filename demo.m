9a96eebcd6f642bde5b7468e5c86321cda5930a4
z21110008 <108250496+z21110008@users.noreply.github.com>
2024-05-29 22:50:45 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,20 @@
+clear;
+clc;
+
+constForVS = 1.27;%fixed
+constForGM = 386;%fixed
+constForChrom = 130;%fixed
+alpha = 0.40;%fixed
+lambda = 0.020;%fixed
+sigmaF = 1.34;%fixed donot change
+omega0 = 0.0210;%fixed
+sigmaD = 145;%fixed
+sigmaC = 0.001;%fixed
+
+img1 = imread('317_1.bmp');
+img2 = imread('317_2.bmp');
+
+UIFscore  = UIF_main(img1,img2)
+
+%UIFscore  = simple_UIF(img1,img2)
+
